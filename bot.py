7990217b4812ae6ec467ff2412e0e3c7f51cec69
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-08-09 23:12:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,7 +64,7 @@ async def approve(_, m: Message):
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
         img = random.choice(gif)
-        await app.send_video(kk.id, img, "**Hello {}😈💜!\nYour request to join🤩 channel {} has been approved✅.keeps sharing and support us🫠🥹\n\n__Powerd By : #SK_MoviesOffl 😎🔥**".format(m.from_user.mention, m.chat.title))
+        await app.send_video(kk.id, img, "**Hello {}😈💜!\nYour request to join🤩 channel {} has been approved✅.keeps sharing and support us🫠🥹\n\n__Powerd By : @SK_MoviesOffl 😎🔥**".format(m.from_user.mention, m.chat.title))
         add_user(kk.id)
     except errors.PeerIdInvalid as e:
         print("user isn't start bot(means group)")
@@ -93,7 +93,7 @@ async def op(_, m: Message):
                 ]
             )
             add_user(m.from_user.id)
-            await m.reply_photo("https://telegra.ph/file/12a39eecaa3031e3c4409.png", caption="**👋 Hello {}!\nAdd This Bot To Your Channel To Accept Join Requests Automatically 😊\n\n__Powerd By : #SK_MoviesOffl**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
+            await m.reply_photo("https://telegra.ph/file/12a39eecaa3031e3c4409.png", caption="**👋 Hello {}!\nAdd This Bot To Your Channel To Accept Join Requests Automatically 😊\n\n__Powerd By : @SK_MoviesOffl**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
 
         elif m.chat.type == enums.ChatType.GROUP or enums.ChatType.SUPERGROUP:
             keyboar = InlineKeyboardMarkup(
@@ -140,7 +140,7 @@ async def chk(_, cb: CallbackQuery):
                 ]
             )
             add_user(cb.from_user.id)
-            await cb.message.edit("**👋 Hello {}!\nAdd This Bot To Your Channel To Accept Join Requests Automatically 😊\n\n__Powerd By : #SK_MoviesOffl**".format(cb.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard, disable_web_page_preview=True)
+            await cb.message.edit("**👋 Hello {}!\nAdd This Bot To Your Channel To Accept Join Requests Automatically 😊\n\n__Powerd By : @SK_MoviesOffl**".format(cb.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard, disable_web_page_preview=True)
         print(cb.from_user.first_name + " Is started Your Bot!")
     except UserNotParticipant:
         await cb.answer("🙅‍♂️ You are not joined our update channel join and try again. 🙅‍♂️")
