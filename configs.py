a0796936f52a6ec79238b5690e7a3030eda4dc9b
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-09-14 11:04:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = int(getenv("API_ID", "23990433"))
     API_HASH = getenv("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")
     BOT_TOKEN = getenv("BOT_TOKEN", "6621256999:AAESnf5zut8Jkt6f4w9r_uCVtfIzJK4pawU")
-    FSUB = getenv("FSUB", "SK_MoviesOffl")
+    FSUB = getenv("FSUB", "")
     CHID = int(getenv("CHID", "-1001842572271"))
     SUDO = list(map(int, getenv("SUDO", "5821871362").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sankar:sankar@sankar.lldcdsx.mongodb.net/?retryWrites=true&w=majority")
