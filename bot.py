eb1bc7fdcb7e5ac8c5341d84a3d84900de33a8d8
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-09-14 13:57:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ async def op(_, m :Message):
                 ]
             )
             add_user(m.from_user.id)
-            await m.reply_photo("https://te.legra.ph/file/288f128f52130adb7ea4f.jpg", caption="**{}\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your Channel Or Group and promote me Admin with add members permission.\n\n__By : @MovieVillaYT__**".format(m.from_user.mention, "https://telegram.dog/MovieVillaYT"), reply_markup=keyboard)
+            await m.reply_photo("https://te.legra.ph/file/288f128f52130adb7ea4f.jpg", caption="**{}\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your Channel Or Group and promote me Admin with add members permission.\n\n__By : @SK_MoviesOffl__**".format(m.from_user.mention, "https://telegram.dog/MovieVillaYT"), reply_markup=keyboard)
     
         elif m.chat.type == enums.ChatType.GROUP or enums.ChatType.SUPERGROUP:
             keyboar = InlineKeyboardMarkup(
@@ -97,7 +97,7 @@ async def chk(_, cb : CallbackQuery):
                 ]
             )
             add_user(cb.from_user.id)
-            await cb.message.edit("**{}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__By : @MovieVillaUpdates__**".format(cb.from_user.mention, "https://telegram.dog/MovieVillaUpdates"), reply_markup=keyboard, disable_web_page_preview=True)
+            await cb.message.edit("**{}!\nI'm an auto approve [Admin Join Requests]({}) Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__By : @SK_MoviesOffl__**".format(cb.from_user.mention, "https://telegram.dog/SK_MoviesOffl"), reply_markup=keyboard, disable_web_page_preview=True)
         print(cb.from_user.first_name +" Is started Your Bot!")
     except UserNotParticipant:
         await cb.answer("🙅‍♂️ You are not joined to channel join and try again. 🙅‍♂️")
